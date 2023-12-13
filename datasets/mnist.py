b0276ab5c60877dea985bb7c35937d07c2a517be
Adam Pickeral <apicker@g.clemson.edu>
2023-12-13 13:09:17 -0500

    --

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -8,49 +8,6 @@ import os
 from .utils import unpickle
 
 
-def read_mnist_images(filename):
-    with open(filename, 'rb') as file:
-        # read the magic number and the number of images
-        magic_number = int.from_bytes(file.read(4), byteorder='big')
-        num_images = int.from_bytes(file.read(4), byteorder='big')
-        num_rows = int.from_bytes(file.read(4), byteorder='big')
-        num_columns = int.from_bytes(file.read(4), byteorder='big')
-
-        # read the image data
-        image_data = np.fromfile(file, dtype=np.uint8)
-        image_data = image_data.reshape(num_images, num_rows, num_columns)
-
-    return image_data
-
-def read_mnist_labels(filename):
-    with open(filename, 'rb') as file:
-        # read the magic number and the number of labels
-        magic_number = int.from_bytes(file.read(4), byteorder='big')
-        num_labels = int.from_bytes(file.read(4), byteorder='big')
-
-        # read the label data
-        label_data = np.fromfile(file, dtype=np.uint8)
-
-    return label_data
-
-
-def load_mnist(root):
-
-    train_images_file = os.path.join(root, 'train-images.idx3-ubyte')
-    train_labels_file = os.path.join(root, 'train-labels.idx1-ubyte')
-    test_images_file = os.path.join(root, 't10k-images.idx3-ubyte')
-    test_labels_file = os.path.join(root, 't10k-labels.idx1-ubyte')
-    x_train = read_mnist_images(train_images_file)
-    y_train = read_mnist_labels(train_labels_file)
-    x_test = read_mnist_images(test_images_file)
-    y_test = read_mnist_labels(test_labels_file)
-    
-    return x_train, y_train, x_test, y_test
-
-
-
-
-
 class MNIST(Dataset):
     
     def __init__(self, root, train):
